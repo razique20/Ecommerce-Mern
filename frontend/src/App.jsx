1041acbb73874989aab7d1523019ebc0eeaa70b7
Razique Mk <raziquemk2002@gmail.com>
2024-08-24 22:33:53 +0530

    Home page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Orders from './pages/Orders'
 import About from './pages/About'
 import Cart from './pages/Cart'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div className='px-4 sm:px[5vw] md:px-[7vw] lg:px-[9vw]'>
@@ -31,6 +32,7 @@ const App = () => {
  
 
       </Routes>
+      <Footer/>
 
     </div>
   )
